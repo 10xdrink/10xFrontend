ae0dca978c168459c810c4587657b8fe3b547d6a
10xdrink <it@10xdrink.com>
2025-04-08 17:36:05 +0530

    Update AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -13,7 +13,7 @@ import CyanBattery from "../assets/CyanBattery.png";
 import FirstCol from "../assets/FirstCol.png";
 import SecondCol from "../assets/SecondCol.png";
 import WithoutX from "../assets/WithoutX.png";
-import ThirdCol from "../assets/MobileAboutResult.png";
+import ThirdCol from "../assets/ThirdCol.png";
 import BlackArrow from "../assets/BlackArrow.png";
 import FaqDown from "../assets/FaqDown.png";
 import FaqUp from "../assets/FaqUp.png";
@@ -24,7 +24,6 @@ import DiscoverCollPurple from "../assets/DiscoverCollPurple.png";
 import About10XIcon2 from "../assets/About10XIcon2.png";
 import About10XIcon1 from "../assets/About10XIcon1.png";
 import About10XIcon3 from "../assets/About10XIcon3.png";
-import DiscoverCollection from "../components/DiscoverCollection";
 
 const AboutUs = () => {
   return (
@@ -104,7 +103,7 @@ const AboutUs = () => {
 
       {/* Our Mission and Vision */}
       <div
-        className="main-vision-mission-div -mt-2 -mb-2 flex transform"
+        className="main-vision-mission-div -mt-2 -mb-2 flex  transform"
         style={{
           backgroundImage: `url(${BluePlainBackgroundRotation})`,
           backgroundSize: "cover",
@@ -213,10 +212,9 @@ const AboutUs = () => {
               business by Kunal Gir, a prominent coach in India. Over two
               decades, Kunal trained thousands of people, including top
               celebrities, and identified a significant problem: nutritional
-              deficiencies affecting people’s mindsets. Recognizing that
-              mindset is fundamental to everything, he decided to distill his
-              knowledge into a convenient, effective solution accessible to
-              everyone.
+              deficiencies affecting people’s mindsets. Recognizing that mindset
+              is fundamental to everything, he decided to distill his knowledge
+              into a convenient, effective solution accessible to everyone.
             </p>
           </div>
           <div className="right w-1/2">
@@ -251,12 +249,13 @@ const AboutUs = () => {
               alt="Green Target"
             />
             <h1 className="text-5xl font-bold uppercase tracking-wider text-white quantico-bold-italic">
-              Kunal's <br /> Previous Ventures
+              Kunal's <br />
+              Previous Ventures
             </h1>
             <p className="text-sm sm:text-base lg:text-lg leading-relaxed pt-sans-regular mt-4 text-white">
-              Include owning supplement stores and developing a supplement
-              line. However, he realized that most nutrients should come from
-              whole foods. Despite this belief, the challenges posed by modern
+              Include owning supplement stores and developing a supplement line.
+              However, he realized that most nutrients should come from whole
+              foods. Despite this belief, the challenges posed by modern
               agriculture and depleted food supplies necessitated a product that
               could be consumed quickly.
             </p>
@@ -301,6 +300,7 @@ const AboutUs = () => {
       </div>
 
       {/* USP's Section */}
+
       <div
         className="usp-section py-10 sm:py-20 px-4 sm:px-6 lg:px-20"
         style={{
@@ -333,6 +333,7 @@ const AboutUs = () => {
                   alt="Decorative Arrow"
                 />
               </div>
+
               <div className="content text-center sm:text-left">
                 <h3 className="text-3xl sm:text-4xl lg:text-5xl font-bold uppercase tracking-wider text-black quantico-bold-italic">
                   CATALYST
@@ -353,6 +354,7 @@ const AboutUs = () => {
                   alt="Decorative Arrow"
                 />
               </div>
+
               <div className="content text-center sm:text-left">
                 <h3 className="text-3xl sm:text-4xl lg:text-5xl font-bold uppercase tracking-wider text-black quantico-bold-italic">
                   EXCELLENCE
@@ -377,6 +379,7 @@ const AboutUs = () => {
                   alt="Decorative Arrow"
                 />
               </div>
+
               <div className="content text-center sm:text-left">
                 <h3 className="text-3xl sm:text-4xl lg:text-5xl font-bold uppercase tracking-wider text-black quantico-bold-italic">
                   CONTROL
@@ -398,6 +401,7 @@ const AboutUs = () => {
                   alt="Decorative Arrow"
                 />
               </div>
+
               <div className="content text-center sm:text-left">
                 <h3 className="text-3xl sm:text-4xl lg:text-5xl font-bold uppercase tracking-wider text-black quantico-bold-italic">
                   POTENTIAL
@@ -427,6 +431,7 @@ const AboutUs = () => {
       <TakeCharge />
 
       {/* Rejuvenate-Section */}
+
       <div
         className="main-div-rejuvenate py-20 px-20"
         style={{
@@ -437,11 +442,12 @@ const AboutUs = () => {
       >
         <div className="top flex">
           <div className="left w-[60%]">
-            <h1 className="text-8xl font-bold uppercase tracking-wider text-white quantico-bold-italic">
+            <h1 className="text-8xl font-bold  uppercase tracking-wider text-white quantico-bold-italic">
               Rejuvenate your <br />
               body with 10x
             </h1>
           </div>
+
           <div className="right w-[40%] flex justify-end">
             <p className="text-xl pt-sans-regular text-white">
               Experience the 10X difference and take on your <br />
@@ -449,6 +455,7 @@ const AboutUs = () => {
             </p>
           </div>
         </div>
+
         <div className="bottom flex mt-12">
           <div className="div-1 w-1/3 p-8">
             <img
@@ -457,15 +464,16 @@ const AboutUs = () => {
               alt=""
             />
             <h3 className="uppercase nimbusL-bol text-white text-2xl">
-              Regenerate your cells
+              Regenerate your cells{" "}
             </h3>
             <p className="text-base pt-sans-regular text-white mt-5">
               Our advanced formula is backed by years of <br />
               research and is proven to regenerate cells,
               <br /> promoting overall health and vitality.
             </p>
+
             <button className="quantico-bold-italic text-white text-lg mt-5 uppercase">
-              learn more <i className="ml-4 fa-solid fa-angle-right"></i>
+              learn more <i class="ml-4 fa-solid fa-angle-right"></i>
             </button>
           </div>
           <div className="div-2 w-1/3 p-8">
@@ -474,16 +482,17 @@ const AboutUs = () => {
               src={About10XIcon1}
               alt=""
             />
-            <h3 className="uppercase nimbusL-bol text-white text-2xl mt-4">
-              the ultimate energy boost
+            <h3 className="uppercase nimbusL-bol text-white text-2xl  mt-4">
+              the ultimate energy boost{" "}
             </h3>
             <p className="text-base pt-sans-regular text-white mt-5">
               Boost your energy levels and stay productive
               <br /> throughout the day with our powerful and natural <br />
               brain battery.
             </p>
+
             <button className="quantico-bold-italic text-white text-lg mt-5 uppercase">
-              learn more <i className="ml-4 fa-solid fa-angle-right"></i>
+              learn more <i class="ml-4 fa-solid fa-angle-right"></i>
             </button>
           </div>
           <div className="div-3 w-1/3 p-8">
@@ -493,25 +502,70 @@ const AboutUs = () => {
               alt=""
             />
             <h3 className="uppercase nimbusL-bol text-white text-2xl mt-12">
-              Unlock your full potential
+              Unlock your full potential{" "}
             </h3>
             <p className="text-base pt-sans-regular text-white mt-5">
               Experience a renewed sense of vitality and unlock
               <br /> your full potential with our revolutionary winner’s
               <br /> shot.
             </p>
+
             <button className="quantico-bold-italic text-white text-lg mt-5 uppercase">
-              learn more <i className="ml-4 fa-solid fa-angle-right"></i>
+              learn more <i class="ml-4 fa-solid fa-angle-right"></i>
             </button>
           </div>
         </div>
       </div>
 
       {/* Discover Collection */}
-      {/* Uncomment if needed:
-      <DiscoverCollection /> 
-      */}
+      <div>
+        <div className="main-div flex w-full">
+          <div
+            className="left-column w-1/2 pt-28 pl-20 pr-20 pb-28 flex flex-col justify-center"
+            style={{
+              backgroundImage: `url(${AvailableAtBG})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            {/* <hr className="horizontal-carousel-hr border-0 bg-white my-2 w-full" /> */}
+            <h1 className="text-8xl font-bold  uppercase tracking-wider text-black quantico-bold-italic">
+              Discover Our <br />
+              Collection of <br />
+              Products
+            </h1>
+            {/* <hr className="horizontal-carousel-hr border-0 bg-white my-2 w-full" /> */}
+            <p className="text-xl pt-sans-regular text-black mt-10">
+              Experience the 10X difference with our expansive range.
+            </p>
 
+            {/* Bottom Buttons */}
+            <div className="flex space-x-4 justify-start mt-10">
+              <div className="learn-more">
+                <button
+                  className="uppercase shadow-[0_4px_10px_rgba(0,0,0,0.3)] border border-[#0821D2] quantico-bold-italic text-xl "
+                  type="button"
+                >
+                  shop now
+                </button>
+                <button
+                  className="uppercase quantico-bold-italic text-xl ml-4"
+                  type="button"
+                >
+                  see all <i class="ml-2 fa-solid fa-angle-right"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="right-column w-1/2">
+            <img className="w-full" src={DiscoverCollPurple} alt="" srcset="" />
+            {/* <img className="-mt-56 -ml-36" src={About10XBottom} alt="" /> */}
+          </div>
+        </div>
+      </div>
+
+      <BlogCard />
     </div>
   );
 };
